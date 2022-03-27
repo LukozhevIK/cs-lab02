d687957d9ba428e178a0605639691ae2a81a7b3c
Alice (LukozhevIK) <LukozhevIK@mpei.ru>
2022-03-27 20:54:41 +0300

    code: изменен тип переменных на вещественный

--- project/project.cpp
+++ project/project.cpp
@@ -8,12 +8,15 @@ using namespace std;
 int main()
 {
     cout << "Enter A and B: ";
-    int a, b;
+    double a, b;
     cin >> a >> b;
     cout << "A + B = " << a + b << '\n'
          << "A - B = " << a - b << '\n'
          << "A * B = " << a * b << '\n'
          << "A / B = " << a / b << '\n';
     if (a > b)
-        cout <<"MAX="<< a;
+        cout << "MAX=" << a;
+    else
+        if (a < b)
+            cout << "MAX=" << b;
 }
